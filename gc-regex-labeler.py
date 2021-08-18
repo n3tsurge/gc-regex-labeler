ff99e057c9d7df13e1c3ec4fee4e8807974cf44a
Carroll, Brian <bcarroll@arifleet.com>
2021-08-17 22:13:46 -0400

    first push

---
+++ gc-regex-labeler.py
@@ -0,0 +1,136 @@
+import logging
+import re
+import json
+from getpass import getpass
+from pyaml_env import parse_config
+from argparse import ArgumentParser
+from guardicore.centra import CentraAPI
+
+def load_config(path="config.yml"):
+    """
+    Loads the configuration file for the application
+    and returns a configuration object for consumption in
+    other areas
+    """
+    config_error = False
+    config = parse_config(path)
+
+    return config
+
+
+def get_nested(message, *args):
+    ''' Iterates over nested fields to get the final desired value '''
+    if args and message:
+        element = args[0]
+        if element:
+            value = message.get(element)
+            return value if len(args) == 1 else get_nested(value, *args[1:])
+
+
+if __name__ == "__main__":
+    # Set the logging format
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    
+    # Parse script parameters
+    parser = ArgumentParser()
+    parser.add_argument('--config', help="The path to the configuration file", default="config.yml", required=False)
+    parser.add_argument('--gc-management-url', help="Guardicore management URL", required=False)
+    parser.add_argument('--report', help="Report the results to a CSV", required=False)
+    parser.add_argument('-u', '--user', help="Guardicore username", required=False)
+    parser.add_argument('-p', '--password', help="Prompt for the Guardicore password", required=False, action="store_true")
+    args = parser.parse_args()
+
+    # Load the configuration
+    config = load_config(path=args.config)
+
+    if args.user:
+        config['guardicore']['username'] = args.user
+
+    if args.password:
+        config['guardicore']['password'] = getpass(prompt="Password: ")
+
+    if args.gc_management_url:
+        config['guardicore']['management_url'] = args.gc_management_url
+
+    # Authenticate to Guardicore
+    logging.info("Authenticating to Guardicore")
+    centra = CentraAPI(management_url=config['guardicore']['management_url'])
+
+    try:
+        centra.authenticate(
+            username=config['guardicore']['username'], password=config['guardicore']['password'])
+    except Exception as e:
+        logging.error(e)
+        exit(1)
+
+    # Fetch all the agents
+    assets = centra.list_assets(limit=100)
+
+    # Create an empty dictionary to store all the labels that were processed
+    # to provide summary metrics at the end of the run
+    labels = {}
+
+    # Run each labeling rule
+    active_rules = [r for r in config['rules'] if config['rules'][r]['enabled']]
+    for rule in active_rules:
+        logging.info(f"Running label rule {rule}")
+
+        rule_config = config['rules'][rule]
+
+        # Check each source field and apply labels based on the labels
+        # defined for that source field
+        for source in rule_config['sources']:
+            
+            source_config = rule_config['sources'][source]
+            source_field = source.split('.')
+
+            for asset in assets:
+                pattern = rule_config['sources'][source]['pattern']
+
+                # Determine if any of the patterns matched at any point
+                matched = False
+
+                # Extract the value from a nested field in the assets dictionary
+                value = get_nested(asset, *source_field)
+
+                if value:
+
+                    # If the target field is a list
+                    # check each value in the list
+                    # or just address the value of the target field
+                    if isinstance(value, list):
+                        
+                        for v in value:
+                            matches = re.match(pattern, v, re.IGNORECASE)
+                    else:
+                        matches = re.match(pattern, value, re.IGNORECASE)
+
+                    if matches:
+                        matched = True
+                        for label in source_config['labels']:
+                            key = label
+                            label_value = source_config['labels'][label]
+
+                            #logging.info(f"Labeling {asset['name']} with {key}: {label_value}")
+
+                            if f"{key}: {label_value}" in labels:
+                                labels[f"{key}: {label_value}"].append(asset['id'])
+                            else:
+                                labels[f"{key}: {label_value}"] = [asset['id']]
+
+                # Add the source field as a label to the asset only if 
+                # the regular patterns matched
+                if 'label_source_field' in source_config and matched:
+                    key = source_config['label_source_field']
+
+                    #logging.info(f"Labeling {asset['name']} with {key}: {value}")
+
+                    if f"{key}: {value}" in labels:
+                        labels[f"{key}: {value}"].append(asset['id'])
+                    else:
+                        labels[f"{key}: {value}"] = [asset['id']]
+
+    # Dedupe the assets in each label
+    labels = {l: list(set(labels[l])) for l in labels}
+    print(json.dumps({l: len(labels[l]) for l in labels}, indent=4))
---
+++ guardicore/centra.py
@@ -0,0 +1,288 @@
+import json
+import requests
+from datetime import datetime, timedelta
+
+class CentraAPI(object):
+
+    def __init__(self, management_url="", http_scheme="https"):
+        """
+        Initializes an API object that is used
+        to make consistent calls to the Guardicore Centra API
+        """
+
+        self.management_url = management_url
+        self.session = requests.Session()
+        self.http_scheme = http_scheme
+        self.base_url = f"{self.http_scheme}://{self.management_url}"
+
+        self.session.headers.update({
+            'Content-Type': 'application/json'
+        })
+
+    def authenticate(self, username, password):
+        """
+        Authenticates to the Guardicore Centra API and 
+        gets back an access_token
+        """
+
+        auth_body = {
+            "username": username,
+            "password": password
+        }
+
+        response = self.session.post(f"{self.base_url}/api/v3.0/authenticate", data=json.dumps(auth_body))
+        if response.status_code == 200:
+            data = response.json()
+
+            # If the account in use has MFA enabled, raise a ValueError
+            if '2fa_temp_token' in data:
+                raise ValueError("Guardicore credentials required MFA.  Use an acccount without MFA.")
+
+            self.session.headers.update({
+                "Authorization": f"Bearer {data['access_token']}"
+            })
+
+        if response.status_code == 401:
+            raise ValueErro("Incorrect Guardicore username or password.")
+    
+    def block_ip(self, ip, rule_set, direction):
+        """
+        Adds an IP address to a policy rule to block
+        traffic to and/or from the IP in question
+        """
+
+        if direction not in ["DESTINATION","SOURCE","BOTH"]:
+            raise ValueError("direction must either be DESTINATION, SOURCE or BOTH")
+
+        if direction in ["DESTINATION", "BOTH"]:
+            data = {
+                "direction": "DESTINATION",
+                "reputation_type": "top_ips",
+                "ruleset_name": rule_set + " | Outbound",
+                "value": ip
+            }
+            self.session.post(f"{self.base_url}/api/v3.0/widgets/malicious-reputation-block", data=json.dumps(data))
+            
+        if direction in ["SOURCE", "BOTH"]:
+            data = {
+                "direction": "SOURCE",
+                "reputation_type": "top_ips",
+                "ruleset_name": rule_set + " | Inbound",
+                "value": ip
+            }
+            self.session.post(f"{self.base_url}/api/v3.0/widgets/malicious-reputation-block", data=json.dumps(data))
+
+
+    def get_incidents(self, tags=[], tag__not=["Acknowledged"], limit=500, from_hours=24):
+        """
+        Fetches a list of incidents from Centra UI based on
+        a set of criteria
+        """
+
+        tag_list = ",".join(tags)
+        tag__not = ",".join(tag__not)
+        from_time = int((datetime.now() - timedelta(hours=from_hours)).timestamp()) * 1000
+        to_time = int(datetime.now().timestamp()) * 1000
+
+        url = f"{self.base_url}/api/v3.0/incidents?tag={tag_list}&tag__not={tag__not}&from_time={from_time}&to_time={to_time}&limit={limit}"
+        response = self.session.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data['objects']
+        else:
+            return []
+
+    def tag_incident(self, id, tags):
+        """
+        Tags an incident with user and system defined
+        tags so analysts can triage a threat more 
+        readily or look back as to why a threat was triaged
+        the way it was 
+        """
+
+        # Assign all the tags
+        for tag in tags:
+            data = {
+                "action": "add",
+                "tag_name": tag,
+                "negate_args": None,
+                "ids": [id]
+            }
+            self.session.post(f"{self.base_url}/api/v3.0/incidents/tag", data=json.dumps(data))
+
+    def acknowledge_incident(self, ids=[]):
+        """
+        Sets the Acknowledged tag on any incidents
+        present in the ids variable
+        """
+
+        # Make sure this is a list
+        if not isinstance(ids, list):
+            raise TypeError("ids should be a list")
+
+        data = {
+            "ids": ids,
+            "negate_args": None
+        }
+        self.session.post(f"{self.base_url}/api/v3.0/incidents/acknowledge", data=json.dumps(data))
+
+    def get_inner(self, destination, source):
+        """
+        Returns the IP that is part of an incident that is actually
+        the bad indicator of the traffic
+        """
+        if destination['is_inner'] == False:
+            return destination['ip']
+        else:
+            return source['ip']
+
+    def insight_query(self, action, query, agent_filter={}):
+        """
+        Runs a Centra Insight query and can wait for the results
+        """
+
+        api_endpoint = "/api/v3.0/agents/query"
+
+        # Raise an error if trying to use an unsupported action value
+        if action not in ["run", "preview_selection", "abort"]:
+            raise ValueError("Invalid action. Must be: run, preview_selection, or abort")
+
+        # Build the post payload
+        data = {
+            "action": action,
+            "filter": agent_filter,
+            "query": query
+        }
+
+        response = self.session.post(f"{self.base_url}{api_endpoint}", data=json.dumps(data))
+        if response.status_code == 200:
+            response_data = response.json()
+            return response_data['id']
+        else:
+            return None
+
+    def insight_query_info(self, query_id, status_only=False):
+        """
+        Returns information about the query ID
+        Important for determining if a query is finished or not
+        status_only will just return the current status
+        if not set this will return the entire response from the API
+        """
+
+        api_endpoint = f"/api/v3.0/agents/query/{query_id}"
+
+        response = self.session.get(f"{self.base_url}{api_endpoint}")
+        if response.status_code == 200:
+            response_data = response.json()
+
+            if status_only:
+                return response_data['status']
+            else:
+                return response_data
+        else:
+            return None
+
+
+    def insight_query_results(self, query_id, limit=20, page=0):
+        """
+        Fetches the result of a completed Insight query
+        """
+
+        # Set the default value for to if not overridden
+        offset = limit * page
+
+        # Create an empty result set
+        results = []
+
+        api_endpoint = f"/api/v3.0/agents/query/{query_id}/results?limit={limit}&offset={offset}"
+
+        response = self.session.get(f"{self.base_url}{api_endpoint}")
+        if response.status_code == 200:
+            response_data = response.json()
+            results += response_data['objects']
+
+            # Page if necessary
+            if page <= round(response_data['total_count']/limit)+1:
+                results += self.insight_query_results(query_id, page=response_data['current_page'])
+
+            return results
+        else:
+            return None
+
+    def insight_label_agents(self, query_id, label_key, label_value, action=""):
+        """
+        Assigns a label to any agent that returned a matching response in the
+        query passed in :query_id:
+        """
+
+        if action not in ["preview_agents_to_label", "add_to_label"]:
+            raise ValueError("Invalid action. Must be: preview_agents_to_label or add_to_label")
+
+        api_endpoint = f"/api/v3.0/agents/query/{query_id}/label"
+
+        label_data = {
+            "action": action,
+            "label_key": label_key,
+            "label_value": label_value
+        }
+
+        response = self.session.post(f"{self.base_url}{api_endpoint}", data=json.dumps(label_data))
+        if response.status_code == 200:
+            response_data = response.json()
+            return response_data
+        else:
+            return None
+
+    def list_agents(self, page=0, limit=20, *args, **kwargs):
+        """
+        Returns a list of agents based on the criteria
+        """
+
+        offset = limit * page
+        
+        api_endpoint = f"/api/v3.0/agents?limit={limit}&offset={offset}"
+
+        # Create an empty set of results
+        results = []
+
+        response = self.session.get(f"{self.base_url}{api_endpoint}")
+        if response.status_code == 200:
+            response_data = response.json()
+            results += response_data['objects']
+
+            # Page if necessary
+            if page <= round(response_data['total_count']/limit)+1:
+                results += self.list_agents(page=response_data['current_page'], limit=limit, *args, **kwargs, )
+
+            return results
+        else:
+            return None
+
+    def list_assets(self, page=0, limit=20, *args, **kwargs):
+        """
+        Returns a list of assets based on the criteria
+        """
+
+        offset = limit * page
+        
+        api_endpoint = f"/api/v3.0/assets?limit={limit}&offset={offset}"
+
+        # Create an empty set of results
+        results = []
+
+        response = self.session.get(f"{self.base_url}{api_endpoint}")
+        if response.status_code == 200:
+            response_data = response.json()
+            results += response_data['objects']
+
+            # Page if necessary
+            if page <= round(response_data['total_count']/limit)+1:
+                results += self.list_assets(page=response_data['current_page'], limit=limit, *args, **kwargs, )
+
+            return results
+        else:
+            return None
+
+        
+
+        
